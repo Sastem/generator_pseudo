35ee7cb9f2a211e26e38c91132b73120256157ba
Sastem <sahai.sarah94@gmail.com>
2021-09-24 16:28:33 +0200

    first commit

---
+++ javascript/generator.js
@@ -0,0 +1,42 @@
+let pseudo = {
+    words : {
+        pseudo_adjectif: [
+            "Mystérieux",
+            "Admirable",
+            "Gentil",
+            "Sot",
+            "Maléfique",
+            "Hilarant"
+        ],
+        pseudo_nom: [
+            "Panda",
+            "Dauphin",
+            "Pavet",
+            "Saucisson",
+            "Carton",
+            "Serpent",
+            "Piano"
+        ]
+    },
+    afficherSurPage: function (chaine) {
+        let resultat = document.getElementById("pseudo");
+        resultat.innerHTML = "<h1 class=\"fw-light\"> Voici votre pseudo : " + chaine + " </h1>";
+        resultat.value = "";
+    },
+    generate: function() {
+        let pseudoGenerated = "";
+        pseudoGenerated += this.words.pseudo_nom[Math.floor((Math.random()*this.words.pseudo_nom
+            .length))];
+        pseudoGenerated+=this.words.pseudo_adjectif[Math.floor((Math.random()*
+            this.words.pseudo_adjectif.length))];
+            console.log(pseudoGenerated);
+        pseudo.afficherSurPage(pseudoGenerated);
+    },
+    init: function () {
+        let _try = document.getElementById("generate-pseudo").addEventListener("click", function (e) {
+            pseudo.generate();
+        });
+    }
+}
+
+pseudo.init();
